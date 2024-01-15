9da3990041bfd1494a9fc8269fcd7a4031b4e5ea
timovdk <5330531+timovdk@users.noreply.github.com>
2024-01-15 17:16:19 +0100

    Refactoring and perf improvements

--- src/soil.py
+++ src/soil.py
@@ -10,29 +10,29 @@ from numba.experimental import jitclass
 
 from repast4py import core, random, space, schedule, logging, parameters
 from repast4py import context as ctx
-import repast4py
 from repast4py.space import DiscretePoint as dpt
+
 
 @numba.jit((int64[:], int64[:]), nopython=True)
 def is_equal(a1, a2):
     return a1[0] == a2[0] and a1[1] == a2[1]
 
+
 spec = [
-    ('xo', int32[:]),
-    ('yo', int32[:]),
-    ('zo', int32[:]),
-    ('xmin', int32),
-    ('ymin', int32),
-    ('zmin', int32),
-    ('ymax', int32),
-    ('xmax', int32),
-    ('zmax', int32)
+    ("xo", int32[:]),
+    ("yo", int32[:]),
+    ("zo", int32[:]),
+    ("xmin", int32),
+    ("ymin", int32),
+    ("zmin", int32),
+    ("ymax", int32),
+    ("xmax", int32),
+    ("zmax", int32),
 ]
 
 
 @jitclass(spec)
 class GridNghFinder:
-
     def __init__(self, xmin, ymin, zmin, xmax, ymax, zmax):
         self.xo = np.array([0, 0, -1, 0, 1, 0, 0], dtype=np.int32)
         self.yo = np.array([0, 1, 0, 0, 0, -1, 0], dtype=np.int32)
@@ -66,26 +66,8 @@ class GridNghFinder:
 
         return np.stack((xs, ys, zs), axis=-1)
 
-def reduce_ratio(ratio_list, total_num, min_num=1):
-    output = [min_num for _ in ratio_list]
-    total_num -= sum(output)
-    if total_num < 0:
-        raise Exception('Could not satisfy min_num')
-    elif total_num == 0:
-        return output
-
-    nloads = len(ratio_list)
-    for ii in range(nloads):
-        load_sum = float( sum(ratio_list) )
-        load = ratio_list.pop(0)
-        value = int( round(total_num*load/load_sum) )
-        output[ii] += value
-        total_num -= value
-    return output
-
 
 class OrganismGroup(core.Agent):
-
     OFFSETS = np.array([-1, 0, 1])
 
     def __init__(self, local_id: int, type: int, rank: int, pt: dpt):
@@ -102,40 +84,9 @@ class OrganismGroup(core.Agent):
         """
         return (self.uid, self.pt.coordinates, self.age, self.biomass)
 
-    def check_death(self, death_age):
-        if self.age >= death_age:
-            return self.uid
-
-    def eat(self, food):
-        self.biomass += food
-
-    # todo: implement biomass cutoff
-    def reproduce(self, reproduction_age):
-        #print('repr')
-        if self.age > reproduction_age:
-            self.age+=1
-            return self.save()
-        else:
-            self.age+=1
-
-    def compete(self, food_opts):
-        self.age+=1
-
-        if food_opts:
-            eat = random.default_rng.choice(food_opts, size=1)[0]
-            return(eat.uid)
-
-    def disperse(self, grid, range):
-        # choose two elements from the OFFSET array
-        # to select the direction to disperse in the
-        # x,y,z dimensions
-        xyz_dirs = random.default_rng.choice(OrganismGroup.OFFSETS, size=3) * range
-        self.pt = grid.move(self, dpt(self.pt.x + xyz_dirs[0], self.pt.y + xyz_dirs[1], self.pt.z + xyz_dirs[2]))
-        
-        self.age+=1
-    
 
 organism_group_cache = {}
+
 
 def restore_organism_group(organism_group_data: Tuple):
     """
@@ -178,26 +129,36 @@ class Model:
         self.context = ctx.SharedContext(comm)
         self.rank = comm.Get_rank()
 
+        # Assign each rank its own seed
+        random.init(rng_seed=params["random.seed"] * (self.rank + 1))
+        self.rng = random.default_rng
+
         # create the schedule
         self.runner = schedule.init_schedule_runner(comm)
         self.runner.schedule_repeating_event(1, 1, self.step)
         self.runner.schedule_repeating_event(1.1, 1, self.log_agents)
-        self.runner.schedule_stop(params['stop.at'])
+        self.runner.schedule_stop(params["stop.at"])
         self.runner.schedule_end_event(self.at_end)
 
-        self.rates = list(params['organism_group.rates'].values())
-        self.organism_range_list = list(params['organism_group.ranges'].values())
-        self.organism_reproduction_age = list(params['organism_group.repr_age'].values())
-        self.organism_death_age = list(params['organism_group.death_age'].values())
-        self.co_occ_network = nx.node_link_graph(json.loads(params['co_occ_network']))
-        self.sorted_occ_net = sorted(self.co_occ_network.degree, key=lambda x: x[1], reverse=True)
-        self.trophic_net = nx.node_link_graph(json.loads(params['trophic_network']))
-        self.width = params['world.width']
-        self.height = params['world.height']
-        self.depth = params['world.depth']
+        # load parameters
+        self.organism_parameters = params["organism_parameters"]
+        self.width = params["world.width"]
+        self.height = params["world.height"]
+        self.depth = params["world.depth"]
 
-        self.nutrient_grid = np.random.rand(self.width, self.height, self.depth)*0.25
+        # load networks
+        self.co_occ_network = nx.node_link_graph(json.loads(params["co_occ_network"]))
+        self.sorted_occ_net = sorted(
+            self.co_occ_network.degree, key=lambda x: x[1], reverse=True
+        )
+        self.trophic_net = nx.node_link_graph(json.loads(params["trophic_network"]))
 
+        # Initialize som grid
+        self.nutrient_grid = (
+            self.rng.random((self.width, self.height, self.depth)) * 0.25
+        )
+
+        # determine the food dependency per organism type
         self.food_dependency = []
         for x in range(len(self.trophic_net.nodes())):
             edges = self.trophic_net.in_edges(nbunch=x)
@@ -208,106 +169,153 @@ class Model:
             self.food_dependency.append(food)
 
         # create a bounding box equal to the size of the entire global world grid
-        box = space.BoundingBox(0, params['world.width']-1, 0, params['world.height']-1, 0, params['world.depth']-1)
+        box = space.BoundingBox(
+            0,
+            params["world.width"] - 1,
+            0,
+            params["world.height"] - 1,
+            0,
+            params["world.depth"] - 1,
+        )
         # create a SharedGrid of 'box' size with sticky borders that allows multiple agents
         # in each grid location.
-        self.grid = space.SharedGrid(name='grid', bounds=box, borders=space.BorderType.Sticky,
-                                     occupancy=space.OccupancyType.Multiple, buffer_size=2, comm=comm)
-        
-        self.ngh_finder = GridNghFinder(0, 0, 0, box.xextent, box.yextent, box.zextent)
-
+        self.grid = space.SharedGrid(
+            name="grid",
+            bounds=box,
+            borders=space.BorderType.Sticky,
+            occupancy=space.OccupancyType.Multiple,
+            buffer_size=2,
+            comm=comm,
+        )
         self.context.add_projection(self.grid)
 
+        # initialize grid searcher
+        self.ngh_finder = GridNghFinder(0, 0, 0, box.xextent, box.yextent, box.zextent)
+
+        # populate the model
         self.populate()
 
         # initialize the logging
-        self.agent_logger = logging.TabularLogger(self.comm, params['agent_log_file'], ['tick', 'agent_id', 'type', 'agent_uid_rank', 'x', 'y', 'z', 'age'])
-        
+        self.agent_logger = logging.TabularLogger(
+            self.comm,
+            params["agent_log_file"],
+            ["tick", "agent_id", "type", "agent_uid_rank", "x", "y", "z", "age", "biomass"],
+        )
+
         self.log_agents()
 
     def populate(self):
-        rng = random.default_rng
-        # calculate no. of agents per rank/process
         ranks = self.comm.Get_size()
-        count_per_rank = int(params['organism_group.count']/ranks)
 
-        # convert ratios of organism_group.types to number of agents for this rank
-        types = dict(params['organism_group.types'])
-        counts = reduce_ratio(ratio_list=list(types.values()), total_num=count_per_rank)
-        
         self.organism_id = 0
         og_types_added = []
 
-        # for each entry i in counts, add v agents
-        for i, v in enumerate(counts):
+        # for each organism group type, add entities
+        for i in range(len(self.organism_parameters)):
             og_type_to_add = self.sorted_occ_net[i][0]
 
+            # number of ogs to add is the initial count divided by the number of ranks
+            num_ogs_to_add = int(
+                self.organism_parameters[og_type_to_add]["count"] / ranks
+            )
+
             # If any of the already added types co-occur with the type we are adding currently, ensure placement is based on co-occurrence
-            if(any([og_type_to_add in self.co_occ_network[p] for p in og_types_added])):
+            if any([og_type_to_add in self.co_occ_network[p] for p in og_types_added]):
                 og_types_added.append(og_type_to_add)
 
                 # Find the types that are already placed
-                intersection = list(set(self.co_occ_network[og_type_to_add]).intersection(og_types_added))
+                intersection = list(
+                    set(self.co_occ_network[og_type_to_add]).intersection(
+                        og_types_added
+                    )
+                )
 
                 co_occurring_agents = []
                 for d in intersection:
                     co_occurring_agents.extend(list(self.context.agents(agent_type=d)))
-                
-                for _ in range(v):
-                    # Get the location of a random co-occurring agent and get a random offset
-                    # offset is based on Von Neumann distance of r=1, including the 0,0,0
-                    co_occ_loc = self.grid.get_location(rng.choice(co_occurring_agents, size=1)[0])
-                    offsets = rng.choice([[0, 0, 1], [0, 1, 0], [-1, 0, 0], [0, 0, 0], [1, 0, 0], [0, -1, 0], [0, 0, -1]], size=1)[0]
-                    
-                    # get a location based on the co_occ_loc and offset, and add an agent here
-                    pt = dpt(min(self.height-1, co_occ_loc.x + offsets[0]), min(self.width-1, co_occ_loc.y + offsets[1]), min(self.depth-1, co_occ_loc.z + offsets[2]))
-                    self.add_agent(og_type_to_add, pt)
-           
+
+                for _ in range(num_ogs_to_add):
+                    choice = self.rng.choice([0, 1], size=1, p=[0.75, 0.25])[0]
+                    if choice == 0:
+                        # Get the location of a random co-occurring agent and get a random offset
+                        # offset is based on Von Neumann distance of r=1, including the 0,0,0
+                        co_occ_loc = self.grid.get_location(
+                            self.rng.choice(co_occurring_agents, size=1)[0]
+                        )
+                        offsets = self.rng.choice(
+                            [
+                                [0, 0, 1],
+                                [0, 1, 0],
+                                [-1, 0, 0],
+                                [0, 0, 0],
+                                [1, 0, 0],
+                                [0, -1, 0],
+                                [0, 0, -1],
+                            ],
+                            size=1,
+                        )[0]
+
+                        # get a location based on the co_occ_loc and offset, and add an agent here
+                        pt = dpt(
+                            min(self.height - 1, co_occ_loc.x + offsets[0]),
+                            min(self.width - 1, co_occ_loc.y + offsets[1]),
+                            min(self.depth - 1, co_occ_loc.z + offsets[2]),
+                        )
+                        self.add_agent(og_type_to_add, pt)
+                    else:
+                        # get a random x,y,z location in the grid and add an agent here
+                        pt = self.grid.get_random_local_pt(self.rng)
+                        self.add_agent(og_type_to_add, pt)
+
             # else add the agents of this type on a random location
             else:
                 og_types_added.append(og_type_to_add)
-                for _ in range(v):
+                for _ in range(num_ogs_to_add):
                     # get a random x,y,z location in the grid and add an agent here
-                    pt = self.grid.get_random_local_pt(rng)
+                    pt = self.grid.get_random_local_pt(self.rng)
                     self.add_agent(og_type_to_add, pt)
 
     def step(self):
-        ogs_to_kill = []
-        # First loop through all organisms to process check_death and nutrient uptake
-        for organism_group in self.context.agents():
-            # First determine whether this organism_group survives
-            uid = organism_group.check_death(self.organism_death_age[organism_group.type])
-            if uid is not None:
-                ogs_to_kill.append(uid)
-        
-        for uid in ogs_to_kill:
-            agent = self.context.agent(uid)
-            if agent is not None:
-                self.remove_agent(agent)
-        
-        self.context.synchronize(restore_organism_group)
-
-        #todo: make nutrient uptake based on monod's eq
-        for organism_group in self.context.agents():
-            # If the organism_group survives, determine nutrient uptake
-            coords = organism_group.pt.coordinates
-            food = self.nutrient_grid[coords[0]][coords[1]][coords[2]]
-            organism_group.eat(food)
-
         ogs_to_add = []
         ogs_to_kill = []
 
-        #todo: make this loop random
-        # Then loop through all organisms to process reproduction/competition/dispersion
+        # First loop through all organisms to process check_death and nutrient uptake
         for organism_group in self.context.agents():
+            organism_parameters = self.organism_parameters[organism_group.type]
+            coords = organism_group.pt.coordinates
+
+            # First determine whether this organism_group survives
+            if organism_group.age >= organism_parameters["age_max"]:
+                ogs_to_kill.append(organism_group.uid)
+                # todo: check whether continue is correct here
+                continue
+
+            # If the organism_group survives, determine nutrient uptake
+            # todo: make nutrient uptake based on monod's eq
+            food = self.nutrient_grid[coords[0]][coords[1]][coords[2]]
+
+            if organism_group.biomass < organism_parameters["biomass_max"]:
+                organism_group.biomass += food
+
             # Pick one of these at random based on rate of events
-            choice = random.default_rng.choice([0, 1, 2], p=self.rates[organism_group.type])
+            choice = self.rng.choice(
+                [0, 1, 2],
+                p=[
+                    organism_parameters["rate_reproduction"],
+                    organism_parameters["rate_competition"],
+                    organism_parameters["rate_dispersal"],
+                ],
+            )
+
             if choice == 0:
-                og = organism_group.reproduce(self.organism_reproduction_age[organism_group.type])
-                if og is not None:
-                    ogs_to_add.append(og)
+                if (
+                    organism_group.age > organism_parameters["age_reproduction"]
+                    and organism_group.biomass
+                    >= organism_parameters["biomass_reproduction"]
+                ):
+                    ogs_to_add.append(organism_group.save())
+
             elif choice == 1:
-                coords = organism_group.pt.coordinates
                 nghs = self.ngh_finder.find(coords[0], coords[1], coords[2])
 
                 food_types = self.food_dependency[organism_group.type]
@@ -319,36 +327,64 @@ class Model:
                         at._reset_from_array(ngh)
 
                         for obj in self.grid.get_agents(at):
-                            if obj.type in food_types:
+                            if obj.type in food_types and obj.uid:
                                 food_opts.append(obj)
-                
-                uid = organism_group.compete(food_opts)
-                if uid is not None:
-                    ogs_to_kill.append(uid)
+
+                if food_opts:
+                    ogs_to_kill.append(self.rng.choice(food_opts, size=1)[0].uid)
 
             else:
-                organism_group.disperse(self.grid, self.organism_range_list[organism_group.type])
+                # choose two elements from the OFFSET array
+                # to select the direction to disperse in the
+                # x,y,z dimensions
+                xyz_dirs = (
+                    self.rng.choice(OrganismGroup.OFFSETS, size=3)
+                    * organism_parameters["range_dispersal"]
+                )
+                organism_group.pt = self.grid.move(
+                    organism_group,
+                    dpt(
+                        coords[0] + xyz_dirs[0],
+                        coords[1] + xyz_dirs[1],
+                        coords[2] + xyz_dirs[2],
+                    ),
+                )
+
+            organism_group.age += 1
 
         for og in ogs_to_add:
-            uid = og[0]
             pt_array = og[1]
-            xyz_dirs = random.default_rng.choice(OrganismGroup.OFFSETS, size=3)
-            
-            pt = dpt(min(self.width-1, pt_array[0] + xyz_dirs[0]), min(self.height-1, pt_array[1] + xyz_dirs[1]), min(self.depth-1, pt_array[2] + xyz_dirs[2]))
-            self.add_agent(uid[1], pt)
+            xyz_dirs = self.rng.choice(OrganismGroup.OFFSETS, size=3)
+
+            pt = dpt(
+                min(self.width - 1, pt_array[0] + xyz_dirs[0]),
+                min(self.height - 1, pt_array[1] + xyz_dirs[1]),
+                min(self.depth - 1, pt_array[2] + xyz_dirs[2]),
+            )
+            self.add_agent(og[0][1], pt)
 
         for uid in list(set(ogs_to_kill)):
             agent = self.context.agent(uid)
             if agent is not None:
                 self.remove_agent(agent)
-        
+
         self.context.synchronize(restore_organism_group)
 
     def log_agents(self):
         tick = int(self.runner.schedule.tick)
         for organism_group in self.context.agents():
             coords = organism_group.pt.coordinates
-            self.agent_logger.log_row(tick, organism_group.id, organism_group.type, organism_group.uid_rank, coords[0], coords[1], coords[2], organism_group.age)
+            self.agent_logger.log_row(
+                tick,
+                organism_group.id,
+                organism_group.type,
+                organism_group.uid_rank,
+                coords[0],
+                coords[1],
+                coords[2],
+                organism_group.age,
+                organism_group.biomass,
+            )
 
         self.agent_logger.write()
 
@@ -357,13 +393,11 @@ class Model:
 
     def start(self):
         self.runner.execute()
-    
+
     def remove_agent(self, agent):
         self.context.remove(agent)
 
     def add_agent(self, type, pt):
-        coords = pt.coordinates
-
         og = OrganismGroup(self.organism_id, type, self.rank, pt)
         self.organism_id += 1
         self.context.add(og)
