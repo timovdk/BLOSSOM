e651fe60bdda04d0a86692e4b930b0c58d5ffe18
timovdk <5330531+timovdk@users.noreply.github.com>
2025-05-05 15:46:30 +0200

    update slurm jobs

--- blossom_cpp/hpc/run_optuna_with_postgres.sh
+++ blossom_cpp/hpc/run_optuna_with_postgres.sh
@@ -10,7 +10,7 @@ module load 2024
 module load Python/3.12.3-GCCcore-13.3.0
 module load PostgreSQL/16.4-GCCcore-13.3.0
 
-source $HOME/venvs/optuna/bin/activate
+source $HOME/BLOSSOM/.venv/bin/activate
 
 PG_DATA="$HOME/pgsql/data"
 LOGFILE="$HOME/pgsql/postgres.log"
@@ -26,18 +26,32 @@ pg_ctl -D $PG_DATA -l $LOGFILE -o "-p $PGPORT" start
 # Wait to ensure the server has started
 sleep 5
 
+# Check if PostgreSQL is ready
+pg_isready -p $PGPORT
+if [ $? -ne 0 ]; then
+    echo "PostgreSQL did not start successfully!"
+    exit 1
+fi
+
 # Create the DB if it doesn't exist
-createdb -p $PGPORT optuna_study
+if ! psql -p $PGPORT -lqt | cut -d \| -f 1 | grep -qw optuna_study; then
+    createdb -p $PGPORT optuna_study
+    echo "Created the optuna_study database."
+else
+    echo "Database optuna_study already exists."
+fi
 
 mkdir -p /scratch-shared/$USER
+
+rm -rf /scratch-shared/$USER/blossom_cpp
 cp -r $HOME/BLOSSOM/blossom_cpp /scratch-shared/$USER
 
 cd /scratch-shared/$USER/blossom_cpp
 
 # Run your Optuna script (which should connect to this DB)
 echo "Running Optuna optimization..."
-python run_optuna.py --n_trials 1000 --n_jobs 4
+python run_optuna.py --n_trials 1000 --n_jobs 23
 
 # Stop PostgreSQL server cleanly
 echo "Stopping PostgreSQL..."
-$PG_BIN/pg_ctl -D $PG_DATA stop
+pg_ctl -D $PG_DATA stop
